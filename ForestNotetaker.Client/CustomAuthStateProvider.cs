48ea3a82a531d03a005b8c3f01f18534eb07e7a8
Chien Tran <mychien1995@gmail.com>
2025-02-10 17:07:36 +0700

    Login with google

---
+++ ForestNotetaker.Client/CustomAuthStateProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace ForestNotetaker;
+
+public class CustomAuthStateProvider(Supabase.Client supabaseClient) : AuthenticationStateProvider
+{
+    private ClaimsPrincipal _currentUser = new(new ClaimsIdentity());
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var session =  supabaseClient.Auth.CurrentSession;
+        if (session?.User != null)
+        {
+            _currentUser = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, session.User.Email!),
+                new Claim(ClaimTypes.NameIdentifier, session.User.Id!)
+            }, "SupabaseAuth"));
+        }
+        else
+        {
+            _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+        }
+        return Task.FromResult(new AuthenticationState(_currentUser));
+    }
+}
--- ForestNotetaker.Client/Program.cs
+++ ForestNotetaker.Client/Program.cs
@@ -1,17 +1,22 @@
 using ForestNotetaker.Business;
 using ForestNotetaker.Business.Services;
+using ForestNotetaker;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var supabaseSetting = new SupabaseSettings(builder.Configuration.GetConnectionString("supabase")!);
+var supabaseSetting = new SupabaseSettings(builder.Configuration.GetConnectionString("Supabase")!);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(supabaseSetting);
 builder.Services.AddSingleton(new Supabase.Client(supabaseSetting.Endpoint, Guid.NewGuid().ToString()));
 builder.Services.AddSingleton<AuthService>();
 builder.Services.AddSingleton<IDataService, DataService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddCascadingAuthenticationState();
 await builder.Build().RunAsync();
